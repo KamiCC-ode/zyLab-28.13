63c091d24aab0b30cee4fb4b17c9c2e37e8873c4
KamiCC-ode <167570218+KamiCC-ode@users.noreply.github.com>
2024-04-19 13:33:23 -0500

    Update ItemToPurchase.h

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,1 +1,15 @@
+#ifndef ITEM_TO_PURCHASE_H
+#define ITEM_TO_PURCHASE_H
 
+#include <string>
+using namespace std;
+
+class ItemToPuchase{
+
+  private:
+  string itemNames;
+  int itemPrice;
+  int itemQuality;
+
+  public;
+  
